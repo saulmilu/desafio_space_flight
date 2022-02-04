bfe95961f8ad63a1b5fb11cacee644142f15fad0
saul milu <saulmsvarao@gmail.com>
2022-02-04 00:30:39 -0300

    ✨ novo componente de busca

---
+++ src/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-text-field
+      name="pesquisa"
+      label="Search"
+      v-model="search"
+      append-icon="mdi-magnify"
+    ></v-text-field>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      search: '',
+    };
+  },
+});
+</script>
+
+<style>
+
+</style>
