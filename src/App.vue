d5d28f89e835294ddd5f35f27e39a69dba0333f5
saul milu <saulmsvarao@gmail.com>
2022-02-04 00:32:21 -0300

    grid e search e filter
    grid para alinhamento dos elementos por area
    
    search para consulta por titulo(a fazer)
    filter por data (a fazer)

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,16 @@
       height="400px"
     >
       <template v-slot:default >
-        <div style="width:100%;" class="d-flex flex-column align-center justify-center">
-          <v-img width="300px" :src="rocket"></v-img>
-          <h2>Space Flight News</h2>
+        <div style="width:100%;" class="grid-3">
+
+          <div  class="d-flex flex-column align-center justify-center grid-3-1">
+            <v-img width="300px" :src="rocket"></v-img>
+            <h2>Space Flight News</h2>
+          </div>
+          <div style="width:100%;" class="grid-3-2 d-flex align-center justify-space-between">
+            <search  class="mr-2"/>
+            <v-select  class="mr-2"  label="Filtrar" :items="items"></v-select>
+          </div>
         </div>
       </template>
     </v-app-bar>
@@ -22,12 +29,31 @@
 <script lang="ts">
 import Vue from 'vue';
 import rocket from './assets/rocket-1206.svg';
+import search from './components/Search.vue';
 
 export default Vue.extend({
   name: 'App',
-
+  components: {
+    search,
+  },
   data: () => ({
     rocket,
+    items: ['antigos', 'recentes'],
   }),
 });
 </script>
+<style lang="scss">
+ .grid-3 {
+   display: grid;
+   grid-template-columns: 30% 1fr 30%;
+ }
+ .grid-3-0 {
+   grid-column: 1/2;
+ }
+ .grid-3-1 {
+   grid-column: 2/3;
+ }
+ .grid-3-2 {
+   grid-column: 3/4;
+ }
+</style>
