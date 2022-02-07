e15f9381cdc3a564c4da55273def534086fbfd7a
saul milu <saulmsvarao@gmail.com>
2022-02-07 00:21:04 -0300

    ⚡️ create store manager

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,8 +5,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    filterTitle: '',
   },
   mutations: {
+    filterTitle(state, payload) {
+      state.filterTitle = payload;
+    },
+  },
+  getters: {
+    filterTitle(state) {
+      return state.filterTitle;
+    },
   },
   actions: {
   },
